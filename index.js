44385513e7d514d21e3928c3914cab1b3b12a2b2
whatsagoku <jefferyshutchinson@gmail.com>
2023-04-30 07:33:02 -0400

    More functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  function wrapAdjective(highlight = '*') {
+    return function(adj = 'special') {
+      return `You are ${highlight}${adj}${highlight}!`;
+    }
+  }
